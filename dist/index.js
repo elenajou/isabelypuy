5007aa680ea8598f16efc513d7f64ee4af1315c3
Elena Jou <elenajou@Elenas-MacBook-Air.local>
2024-05-26 19:19:37 -0700

    first commit

---
+++ dist/index.js
@@ -0,0 +1,49 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = __importDefault(require("dotenv"));
+const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
+const cors_1 = __importDefault(require("cors"));
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+// Array of example users for testing purposes
+const users = [
+    {
+        id: 1,
+        name: 'Maria Doe',
+        email: 'maria@example.com',
+        password: 'maria123'
+    },
+    {
+        id: 2,
+        name: 'Juan Doe',
+        email: 'juan@example.com',
+        password: 'juan123'
+    }
+];
+app.use(express_1.default.json());
+app.use((0, cors_1.default)());
+if (process.env.NODE_ENV === 'production') {
+    app.use(express_1.default.static(path_1.default.join(__dirname, '../client/build')));
+    app.get('/*', function (req, res) {
+        res.sendFile(path_1.default.join(__dirname, '../client/build', 'index.html'));
+    });
+}
+// route login
+app.post('/login', (req, res) => {
+    const { email, password } = req.body;
+    const user = users.find(user => {
+        return user.email === email && user.password === password;
+    });
+    if (!user) {
+        return res.status(404).send('User Not Found!');
+    }
+    return res.status(200).json(user);
+});
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
---
+++ index.ts
@@ -0,0 +1,59 @@
+import dotenv from "dotenv";
+import express, { Express, Request, Response } from "express";
+import path from "path";
+import cors from "cors";
+
+dotenv.config();
+
+const app: Express = express();
+interface FormInputs {
+  email: string,
+  password: string
+}
+
+// Array of example users for testing purposes
+const users = [
+  {
+    id: 1,
+    name: 'Maria Doe',
+    email: 'maria@example.com',
+    password: 'maria123'
+  },
+  {
+    id: 2,
+    name: 'Juan Doe',
+    email: 'juan@example.com',
+    password: 'juan123'
+  }
+];
+
+app.use(express.json());
+app.use(cors());
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+
+  app.get('/*', function (req: Request, res: Response) {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  });
+}
+
+// route login
+app.post('/login', (req: Request, res: Response) => {
+  const { email, password }:FormInputs = req.body;
+
+  const user = users.find(user => {
+    return user.email === email && user.password === password
+  });
+
+  if (!user) {
+    return res.status(404).send('User Not Found!')
+  }
+
+  return res.status(200).json(user)
+});
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
